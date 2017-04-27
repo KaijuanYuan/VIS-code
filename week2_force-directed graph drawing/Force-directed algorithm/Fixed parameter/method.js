0d9e83c1f68e139e35930c4d751f0224810bc86c
Kaijuan Yuan <yuankaijuan@163.com>
2017-04-27 20:25:45 +0800

    modify the farce-directed algorithm

--- week2_force-directed graph drawing/Force-directed algorithm/Fixed parameter/method.js
+++ week2_force-directed graph drawing/Force-directed algorithm/Fixed parameter/method.js
@@ -1,7 +1,23 @@
-var connection = [[0, 1, 1], 
+var connection =[[0, 1, 1, 1, 1, 1], 
+				 [1, 0, 1, 1, 1, 1], 
+				 [1, 1, 0, 1, 1, 1],
+				 [1, 1, 1, 0, 1, 1],
+				 [1, 1, 1, 1, 0, 1],
+				 [1, 1, 1, 1, 1, 0]];
+				 
+				 /*[[0, 1, 1], 
 				 [1, 0, 1], 
-				 [1, 1, 0]];
-				 /*[[0, 1, 0, 1, 1, 0, 0, 0], 
+				 [1, 1, 0]];*/
+				 /* [[0, 1, 0, 1], 
+				 [1, 0, 1, 0], 
+				 [0, 1, 0, 1],
+				 [1, 0, 1, 0]];*/
+				/* [[0, 1, 1, 1, 1], 
+				 [1, 0, 1, 1, 1], 
+				 [1, 1, 0, 1, 1],
+				 [1, 1, 1, 0, 1],
+				 [1, 1, 1, 1, 0]];*/
+				/*  [[0, 1, 0, 1, 1, 0, 0, 0], 
 				 [1, 0, 1, 0, 0, 1, 0, 0], 
 				 [0, 1, 0, 1, 0, 0, 1, 0], 
 				 [1, 0, 1, 0, 0, 0, 0, 1],
@@ -9,19 +25,13 @@ var connection = [[0, 1, 1],
 				 [0, 1, 0, 0, 1, 0, 1, 0],
 				 [0, 0, 1, 0, 0, 1, 0, 1],
 				 [0, 0, 0, 1, 1, 0, 1, 0]];*/
-				/* [[0, 1, 1], 
-				 [1, 0, 1], 
-				 [1, 1, 0]];*/
-
-
+				
+				 
 
 var origin = {x:0, y:0};
-var node1 = {x:0, y:0};
-var node2 = {x:100, y:0};
-var node3 = {x:50, y:86};
-var node4 = {x:30, y:-30};
+
 var vectorC = {magnitude:0, direction:0};
-//var diagram = [node1, node2, node3];
+
 var diagram = [];
 var next = [];
 var REPULSION_CONSTANT = 10000;
@@ -29,7 +39,7 @@ var ATTRACTION_CONSTANT = 0.1;//0.1
 var SPRING_LENGTH = 80;
 var ITERATION = 500;//500 threshold<10
 var DAMPLING = 0.5;//0.5
-var N = 0;
+var N = 6;
 
 function calDistance(nodeX, nodeY) {
 	return Math.sqrt(Math.pow((nodeX.x - nodeY.x ), 2) + Math.pow((nodeX.y - nodeY.y), 2 ));
@@ -102,13 +112,23 @@ function generatePosition(){
 
 	}
 
-	for(var i = 0; i < diagram.length; i++) {
+	var initialPosition = "";
+	for(var i = 0; i < N; i++) {
 		//document.write(diagram[i].x + " " + diagram[i].y + "</br>");
+		initialPosition += diagram[i].x + " " + diagram[i].y + "<br>";
+		
 	}
+
+	//document.getElementById("initialPos").innerHTML = initialPosition;
 	
 	
 }
 
+
+function sim(n1,n2) {
+	if(Math.abs(n1.x-n2.x)<0.5 && Math.abs(n1.y-n2.y)<0.5)
+		return true;
+}
 
 function forceDirected(){
 	diagram = [];
@@ -152,22 +172,27 @@ function forceDirected(){
 			diagram[i].y = next[i].y;
 			//document.write("New position: " + diagram[i].x + " " + diagram[i].y + "</br>" + "</br>");
 		}
-		if(totalDisplacement < 1)
+
+		if(totalDisplacement < 10)
 		{
-			//document.write(loop);
-			break;
+			var flg = false;
+			for(var i=0; i<N; i++) 
+				for(var j=i+1; j<N; j++) 
+					if(sim(diagram[i], diagram[j]))
+						flg = true;
+				
+			if(flg == false)
+				break;
 		}
 		
 
 	}
 
-	/*for(var i = 0; i < diagram.length; i++) {
-		document.write(diagram[i].x + " " + diagram[i].y + "</br>");
-	}*/
-
-	//var st = diagram[1].x + "" + diagram[1].y + "" + diagram[2].x + "" + diagram[2].y + "" + diagram[3].x + "" + diagram[3].y + "" + diagram[4].x + "" + diagram[4].y;
 	return 0;
 }
+
+
+
 
 function boundary() {
 	var miniX = 0;
@@ -186,36 +211,33 @@ function boundary() {
 	}
 
 	//document.write(miniX + " " + miniY + "</br>");
-	if((miniX<0) || (miniY<0))
-		for(var i=0; i<N; i++) {
+	if(miniX<0)
+		for(var i=0; i<N; i++) 
 			diagram[i].x = diagram[i].x + Math.abs(miniX);
+
+
+	if(miniY<0)
+		for(var i=0; i<N; i++) 
 			diagram[i].y = diagram[i].y + Math.abs(miniY);
-		}
+		
 
 	if(maxiX>1000)
 		for(var i=0; i<N; i++) 
-			diagram[i].x = diagram[i].x - (maxiX-1000);
+			diagram[i].x = diagram[i].x - (maxiX-1200);
 			
 
 	if(maxiY>500)
 		for(var i=0; i<N; i++) 
-			diagram[i].y = diagram[i].y - (maxiY-500);
+			diagram[i].y = diagram[i].y - (maxiY-600);
 		
-
-	/*for(var i=0; i<N; i++) {
-		for(var j=0; j<N; j++) {
-			document.write(diagram[i].x + "  " + diagram[i].y + "<br>");
-		}
-		
-	}*/
-
 	return;
 }
 
 
+
 function draw(canId){
 
-//document.write((180.0 / Math.PI) * Math.atan2(-1000, -1000));
+	N = connection.length;
 	forceDirected();
 
 	var c = document.getElementById(canId);
@@ -223,8 +245,13 @@ function draw(canId){
 	ctx.clearRect(0, 0, 1000, 500); 
 
 	boundary();
+	/*var st = N + "<br>";
+	for(var i=0; i<N; i++) {
+		st += diagram[i].x + " " + diagram[i].y + "<br>";
+		document.getElementById("initialPos").innerHTML = st;
+	}*/
 
-	for(var i=0; i<diagram.length; i++) {
+	for(var i=0; i<N; i++) {
 		ctx.beginPath();
 		ctx.strokeStyle = "blue";
 		ctx.arc(diagram[i].x,diagram[i].y,5,0,2*Math.PI);
@@ -233,8 +260,8 @@ function draw(canId){
 
 	}
 
-	for(var i=0; i<diagram.length; i++) {
-		for(var j=i+1; j<diagram.length; j++) {
+	for(var i=0; i<N; i++) {
+		for(var j=i+1; j<N; j++) {
 			if(connection[i][j]!=0) {
 				ctx.strokeStyle = "blue";
 				ctx.moveTo(diagram[i].x, diagram[i].y);
@@ -244,9 +271,7 @@ function draw(canId){
 		}
 	}
 
-	
-	
-	
+		
 }
 
 
